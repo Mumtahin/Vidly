832eceb4caa05d7dc28840e5b7c853de3527db20
Nazmul Islam Mumtahin <n.mumtahin1@gmail.com>
2019-04-17 00:26:03 +0100

    add basic movies controller with movies list

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,16 +4,45 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            var movie = new Movie(1, "Shrek!");
+            var customers = new List<Customer>
+            {
+                new Customer(1, "Customer 1"),
+                new Customer(2, "Customer 2")
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie(1, "Shrek"),
+                new Movie(2, "Tarzan")
+            };
         }
     }
 }
