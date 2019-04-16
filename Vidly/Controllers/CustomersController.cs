2bd3d84cb72b73eb09b1a82d65b3c08d50c62b1d
Nazmul Islam Mumtahin <n.mumtahin1@gmail.com>
2019-04-17 00:25:03 +0100

    add basic customer controller with customer list

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ViewResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer ( 1, "John Smith"),
+                new Customer ( 2, "Mary Williams")
+            };
+        }
+    }
+}
