0deafc64b3ed8c99760888c6b74491a3ae82fff7
Nazmul Islam Mumtahin <n.mumtahin1@gmail.com>
2019-04-23 12:12:14 +0100

    add membershiptype model and update customer model

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
-using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -15,25 +14,6 @@ namespace Vidly.Controllers
             var movies = GetMovies();
 
             return View(movies);
-        }
-
-        // GET: Movies/Random
-        public ActionResult Random()
-        {
-            var movie = new Movie(1, "Shrek!");
-            var customers = new List<Customer>
-            {
-                new Customer(1, "Customer 1"),
-                new Customer(2, "Customer 2")
-            };
-
-            var viewModel = new RandomMovieViewModel()
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(viewModel);
         }
 
         private IEnumerable<Movie> GetMovies()
